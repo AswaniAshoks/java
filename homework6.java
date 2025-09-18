a64b91405881a75d3d328dc5bd8ad1876a78d985
Aswani S <aswaniashok2810@gmail.com>
2025-09-18 13:29:55 +0530

    gradecard

---
+++ homework6.java
@@ -0,0 +1,34 @@
+public class homework6 {
+   public static void main(String[] args) {
+        
+        int[] marks = {78, 82, 91, 65, 34}; 
+
+        int total = 0;
+        
+
+        
+        for (int mark : marks) {
+            if (mark < 35) {
+                  System.out.println(" Result=Fail");
+            }
+            total += mark;
+        }
+
+        int average =total/marks.length;
+          System.out.println("Total Marks: " + total);
+            System.out.println("Average Marks: " + average);
+
+            if (average >= 90) {
+                System.out.println("Grade: A");
+            } else if (average >= 75) {
+                System.out.println("Grade: B");
+            } else if (average >= 60) {
+                System.out.println("Grade: C");
+            } else {
+                System.out.println("Grade: D");
+            }
+        }
+    }
+
+
+
