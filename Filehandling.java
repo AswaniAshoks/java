91dfc7de6a3bc73a278c94d45937a6886ec596d1
Aswani S <aswaniashok2810@gmail.com>
2025-09-25 19:57:21 +0530

    Filehandling

--- Filehandling.java
+++ Filehandling.java
@@ -1,39 +1,49 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 public class Filehandling {
     public static void main(String[] args) {
         String fileName = "assignment.txt";
-        Path path = Paths.get(fileName);
         String content = "Java File Handling Practice";
-
+        File file = new File(fileName);
 
         try {
-            Files.write(path, content.getBytes());
-            System.out.println(" File '" + fileName + "' created and content written successfully.");
+            if (file.createNewFile()) {
+                System.out.println(fileName + "' created successfully.");
+            } else {
+                System.out.println( fileName + "' already exists.");
+            }
+        } catch (Exception e) {
+            System.out.println(" Error creating the file: " + e.getMessage());
+            return;
+        }
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(content);
+            writer.close();
+            System.out.println("Content written to the file.");
         } catch (Exception e) {
             System.out.println(" Error writing to the file: " + e.getMessage());
             return;
         }
-
-
         try {
-            String readContent = Files.readString(path);
-            System.out.println(" Reading content from '" + fileName + "':");
-            System.out.println(readContent);
-            System.out.println(" File read successfully.");
+            FileReader reader = new FileReader(file);
+            int ch;
+            System.out.print(" File content: ");
+            while ((ch = reader.read()) != -1) {
+                System.out.print((char) ch);
+            }
+            reader.close();
+            System.out.println("File read successfully.");
         } catch (Exception e) {
-            System.out.println(" Error reading the file: " + e.getMessage());
+            System.out.println("Error reading the file: " + e.getMessage());
             return;
         }
-
-        
-        try {
-            Files.delete(path);
-            System.out.println(  fileName + "' deleted successfully.");
-        } catch (Exception e) {
-            System.out.println(" Failed to delete the file: " + e.getMessage());
+        if (file.delete()) {
+            System.out.println( fileName + "' deleted successfully.");
+        } else {
+            System.out.println("Failed to delete the file.");
         }
     }
 }
